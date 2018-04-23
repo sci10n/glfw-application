f04fb13a8bb437cb2685594ca456532f0183d49c
Martin Estgren <mares480@student.liu.se>
2018-04-24 00:13:43 +0200

    Changed workspace description to allow for mulltiple libs. Also added support for GLFW, requires precompiled in Libraries/GLFW

--- premake5.lua
+++ premake5.lua
@@ -2,9 +2,9 @@ name = "project"
 workspace (name)
     language "C++"
     location "build"
+    architecture "x86_64"
     warnings "Extra"
-    cppdialect "C++14"
-    -- platforms {"Static", "Shared"}
+    cppdialect "C++11"
     configurations {"Debug", "Release"}
 
     filter {"configurations:Debug"}
@@ -17,45 +17,65 @@ workspace (name)
         optimize "Speed"
         symbols "Off"
 
------- Program
-project (name)
-    targetdir "bin"
-    kind "WindowedApp"
-    files {"src/main.cc"}
-    files {"foreign/src/**.c"}
-    files {"foreign/src/**.cpp"}
-    files {"src/"..name.."/**.cc"}
-    includedirs {"foreign/include"}
-    includedirs {"include"}
-    -- links {"library"}
+-- This function includes GLFW's header files
+function includeGLFW()
+    includedirs "Libraries/GLFW/include"
+end 
 
--- ------ Library
--- project (name)
---     targetdir "lib"
---     files {"foreign/src/**.c"}
---     files {"foreign/src/**.cpp"}
---     files {"src/"..name.."/**.cc"}
---     includedirs {"foreign/include"}
---     includedirs {"include"}
---     -- links {"library"}
--- 
---     filter {"platforms:Static"}
---         defines {"STATIC"}
---         kind "StaticLib"
--- 
---     filter {"platforms:Shared"}
---         defines {"SHARED"}
---         kind "SharedLib"
+-- This function links statically against GLFW
+function linkGLFW()
+    libdirs "Libraries/GLFW/lib"
+    
+    -- Our static lib should not link against GLFW
+    filter "kind:not StaticLib"
+        links "glfw3"
+    filter {}
+end
 
----------------- Testing
-project (name.."-tests")
-    targetdir "bin"
+-- Our first project, the static library
+project "ExampleLib"
+    kind "StaticLib"
+    files "Projects/ExampleLib/**"
+    includeGLFW()
+
+function useExampleLib()
+    includedirs "Projects/ExampleLib"
+    
+    -- We link against a library that's in the same workspace, so we can just
+    -- use the project name - premake is really smart and will handle everything for us.
+    links "ExampleLib"
+    linkGLFW()
+end
+
+-- The windowed app
+project "App"
     kind "ConsoleApp"
-    files {"tests/main.cc"}
-    files {"foreign/src/**.c"}
-    files {"foreign/src/**.cpp"}
-    files {"src/"..name.."/**.cc"}
-    files {"tests/"..name.."**.cc"}
-    includedirs {"foreign/include"}
-    includedirs {"include"}
-    -- links {"library"}
+    files "Projects/App/**"
+
+    includedirs "Projects/ExampleLib"
+    
+    useExampleLib()
+        
+    filter { "system:windows" }
+    links { "OpenGL32" }
+        
+    filter { "system:not windows" }
+    links { "GL" }
+
+-- We will use this function to include Catch
+function includeCatch()
+    -- Catch is header-only, we need just the Catch.hpp header
+    includedirs "Libraries/Catch/include"
+    
+    -- We can also configure Catch through defines
+    defines "CATCH_CPP11_OR_GREATER"
+end
+
+project "UnitTests"
+    -- Catch prints information to the console
+    kind "ConsoleApp"
+    
+    files "Projects/UnitTests/**"
+    
+    includeCatch()
+    useExampleLib()
